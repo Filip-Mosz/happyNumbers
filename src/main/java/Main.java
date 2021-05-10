fe7dc1e1f648e1170de84a6e26d739100e1d3fc7
FilipM <filip.moszczynski@onet.pl>
2021-05-10 19:34:50 +0200

    4 tests - 2 unit, 2 parameterized

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
+    //helpful: http://www.deltami.edu.pl/temat/matematyka/teoria_liczb/2015/03/31/Wesole_liczby/
 }
--- src/test/java/FinderTest.java
+++ src/test/java/FinderTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,6 +9,23 @@ public class FinderTest {
     @Test
     public void isHappy(){
         assertTrue(Finder.find(13));
+    }
+
+    @Test
+    public void isSad(){
+        assertFalse(Finder.find(85));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints={226, 280, 301, 319, 622, 635, 899, 100})
+    public void areHappy(int number){
+        assertTrue(Finder.find(number));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints={0, 5, 30, 191, 200, 380, 403, 505, 999})
+    public void areSad(int number){
+        assertFalse(Finder.find(number));
     }
 
 }
