ef17406f2527e9ccb965494b7ab96c607ec11e5d
FilipM <filip.moszczynski@onet.pl>
2021-05-13 19:54:43 +0200

    i messed something. 2 private methods made public, to test

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Some time ago on JoeMonster i read about happy numbers https://en.wikipedia.org/
 It's a perfect excercise after working with Spring, something light, no pressure.
 
 ### Tactic Plan
-1. method that checks if given number is Happy
-1.1 tdd
+~~1. method that checks if given number is Happy
+1.1 tdd~~
 1.2 working method ignoring Big-O
 2. terminal interface
 2.1 after each result ask if user wants to try again or try something else
@@ -14,3 +14,8 @@ It's a perfect excercise after working with Spring, something light, no pressure
 4. finding closest Happy Number to number given by user
 5. finding all happy Numbers in range given by user
 6. method for finding happy Prime Numbers for all options
+?. numbers n-happy (happy but when raised not to 2nd but n-th power)
+
+I'm going to try TDD; I never really kept that principle, time was always short :( (so my skill)
+
+Second thing to do is optimisation for better big-O notation result
--- src/main/java/Finder.java
+++ src/main/java/Finder.java
@@ -1,6 +1,88 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 public class Finder {
 
-    public static boolean find(int number){
-        return true;
+    public static boolean find(int number) {
+
+        Set<Integer> setOfResults = new HashSet<>();
+
+        if (number == 0) {
+            return false;
+        }
+//add to set
+//        if set.add() returns true, keep going
+//        if set.add() returns false, number is sad
+        int a1 = 0;
+
+        do{
+        a1 = squareEachDigitAndSum(number);
+            if (a1 == 1){
+                return true;
+            }
+        }while (setOfResults.add(a1));
+
+        return false;
     }
+
+    public static int squareEachDigitAndSum(int number) {
+// number to String
+//        then String.length(), which gives length of table for digits
+//        then each character to int and square then to table
+//        finally sum everything and return
+
+        String stringifiedNumber = String.valueOf(number);
+        List<Integer> listOfDigits = new LinkedList<>();
+        int sum = 0;
+
+//        for(int i=0; i < stringifiedNumber.length(); i++){
+//            listOfDigits.add(squareExceptZero(Integer.getInteger(String.valueOf(stringifiedNumber.charAt(i)))));
+//            System.out.println("tabela: (powinna mieć 1 wpis)" + listOfDigits);
+//        }
+        for(int i=0; i < stringifiedNumber.length(); i++){
+            sum+= squareExceptZero(Integer.getInteger(String.valueOf(stringifiedNumber.charAt(i))));
+            System.out.println("digit added");
+        }
+//wrzuca do listy, ale czy potrzebnie? Zmienna plus wynik metody potęgującej lepszy?
+        //tu skończyłem, straciłem wątek :/
+
+        return sum;
+    }
+
+    public static int squareExceptZero(int number){
+//        easier that way than catching exception
+        if(number == 0){
+            return 0;
+        }
+        else {
+            return (int) Math.pow(number, 2);
+        }
+    }
+
+    //liczba jest wesoła gdy:
+//    suma kwadratów jej składników w końcu zmierza do 1
+//    czuli składa się tylko z 1 i/lub 0
+
+//    Jeśli wynik zacznie się powtarzać liczba jest smutna
+
+//    Czym więc są liczby wesołe? Wybierzmy liczbę naturalną >0 N i rozważmy następujący algorytm.
+//
+//(1)
+//    Oblicz sumę kwadratów cyfr wchodzących w skład zapisu liczby a0 = N i oznacz ją przez a1.
+//(2)
+//    Jeżeli | a1 = 1, zakończ algorytm. Jeżeli | a1 ≠1, przejdź do następnego kroku.
+//(3)
+//    Mając już | a0,a1,...,ak dla pewnego | k ⩾ 1, zastosuj krok pierwszy do liczby ak - otrzymasz liczbę |ak+1.
+//(4)
+//    Jeżeli |ak+1 = ai≠ 1 dla pewnego i ∈{0,...,k}, zakończ algorytm. W przeciwnym przypadku kontynuuj algorytm, przechodząc do kroku (2),
+//    który stosujesz dla liczby |ak+1.
+//
+//Jeżeli w kroku (2) dla pewnego k ⩾1 zachodzi a = 1, k to liczbę N nazwiemy wesołą.
+// Jeżeli prawdziwy jest warunek ak+1 = ai ≠1 dla pewnego |i∈{0, ...,k}, to liczbę |N nazwiemy smutną.
+// W celu lepszego zrozumienia postawionej wyżej definicji przyjrzyjmy się kilku przykładom.
+//
+//
+//Weźmy liczbę 133. Krok (1) mówi nam "oblicz sumę kwadratów cyfr". W naszym przypadku jest to |12 + 32 + 32 = 19, a więc liczba różna od 1. Warunek z kroku (2) nie jest spełniony, przechodzimy więc do kroku (3). Zgodnie z krokiem (3) obliczamy teraz sumę kwadratów cyfr liczby 19. Otrzymujemy 12 + 92 = 82. Przechodząc do kroku (4), stwierdzamy, że warunek nie jest spełniony, a więc należy wrócić do kroku (2). Wykonując algorytm, otrzymujemy kolejno 68, 100, 1. Ostatecznie po pięciu powtórzeniach otrzymaliśmy jedynkę, co oznacza, że liczba 133 jest wesoła.
 }
